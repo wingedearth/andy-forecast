7043391fa9d2a3b6bbdea43b50a5ab6eb0b26ff9
Andrew A. Anissi <andrew@wingedearth.com>
2023-10-04 11:29:49 -0500

    feat: add build with babeljs

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const quote1 = '"Nothing great was ever achieved without enthusiasm." - Ralph Waldo Emerson';
+const quote2 = '"Regard it as just as desirable to build a chicken house as to build a cathedral." - Frank Lloyd Wright';
+
+const start = () => {
+    console.log(quote1);
+    console.log(quote2);
+};
+
+start();
